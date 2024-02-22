41d3dc1c03c9c462ab54f2f0f9e6821c48068eab
Dhruv Mehta <flashxx001@gmail.com>
2024-02-22 14:10:36 +0530

    Completed While and Do While Loop

---
+++ 05_iterations/two.js
@@ -0,0 +1,24 @@
+// While Loop
+
+// let index = 0
+// while (index <=20) {
+//     console.log(`Satisfied, Value is ${index}`);
+//     index = index + 5
+    
+// }
+
+// let myArray = ["Shaktimaan", "Flash", "Batman", "Ironman"]
+// let arr = 0
+// console.log(myArray.length);
+
+// while (arr < myArray.length) {
+//     console.log(`${myArray[arr]}`);
+//     arr = ++arr
+// }
+
+let score = 1
+
+do {
+    console.log(`Score is: ${score}`);
+    score++
+} while (score <= 10);
