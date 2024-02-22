ed7673f294b9ceecc10ea45f0be7dedd2449e6ff
Dhruv Mehta <flashxx001@gmail.com>
2024-02-22 15:06:58 +0530

    Completed Different types of loops

---
+++ 05_iterations/four.js
@@ -0,0 +1,38 @@
+// const coding = ["Js", "Python", "Ruby", "CPP"]
+
+// coding.forEach( (item, index, arr) => {
+//     console.log(item, index, arr);
+// })
+
+// coding.forEach( function (item) {
+//     console.log(item);
+// })
+
+// coding.forEach((val) =>{
+//     console.log(val);
+// })
+
+// function printMe(item){
+//     console.log(item);
+// }
+
+// coding.forEach(printMe)
+
+const myCoding = [
+    {
+        languageName: "Javascript",
+        languageFile: "js"
+    },
+    {
+        languageName: "java",
+        languageFile: "java"
+    },
+    {
+        languageName: "python",
+        languageFile: "py"
+    }
+]
+
+myCoding.forEach((item) => {
+    console.log(item.languageName);
+})
---
+++ 05_iterations/three.js
@@ -0,0 +1,76 @@
+// for of
+
+// ["Dhruv", "Kartik", "Aarav"]
+
+// [{}, {}, {}]
+
+// const arr = [1, 2, 3, 4, 5]
+
+// for (const val of arr) {
+//     console.log(val);
+// }
+
+const greetings = "Hello World!"
+for (const greet of greetings) {
+    // console.log(greet);
+}
+
+// Maps
+
+// const map = new Map()
+// map.set('IN', "India")
+// map.set('USA', "United States of America")
+// map.set('IN', "India")
+// map.set('FR', "France")
+
+// // console.log(map);
+
+// for (const [key, value] of map) {
+//     console.log(key + ": " + value);
+// }
+
+
+// To Access either key or value
+
+// const map = new Map()
+// map.set('IN', "India")
+// map.set('USA', "United States of America")
+// map.set('IN', "India")
+// map.set('FR', "France")
+
+// console.log(map);
+
+// for (const key of map.values()) {
+//     console.log(key);
+// }
+
+
+// const myObject = {
+//     'game1': 'NFS',
+//     'game2': 'Spiderman'
+// }
+
+// for (const testObject of Object.entries(myObject)) {
+//     console.log(testObject);
+// }
+
+
+// For in
+
+// const myObject = {
+//     js: 'Javascript',
+//     cpp: 'C++',
+//     rb: 'Ruby'
+// }
+
+// for (const key in myObject) {
+//     console.log(`Shortcut for ${myObject[key]} is -> ${key}`);
+// }
+
+// const programming = ["Javascript", "Ruby", "C++", "Python"]
+
+
+// for (const key in programming) {
+//         console.log(programming[key]);
+
+// }
