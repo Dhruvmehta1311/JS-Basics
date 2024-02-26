57e1bd50473244e3ec434635afca4b9384c102cb
Dhruv Mehta <flashxx001@gmail.com>
2024-02-26 18:38:49 +0530

    Minor Change..

--- 07_projects/01_project/script.js
+++ 07_projects/01_project/script.js
@@ -7,18 +7,10 @@ buttons.forEach(function(button){
         console.log(e);
         // console.log(e.target);
 
-        if (e.target.id === "grey") {
+        if (e.target.id === e.target.id) {
             body.style.backgroundColor = e.target.id
         }
-        if (e.target.id === "white") {
-            body.style.backgroundColor = e.target.id
-        }
-        if (e.target.id === "yellow") {
-            body.style.backgroundColor = e.target.id
-        }
-        if (e.target.id === "blue") {
-            body.style.backgroundColor = e.target.id
-        }
+
 
 
     } )
