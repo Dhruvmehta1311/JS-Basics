7e27f5cba8a61e594bf0eee759d7a147d00362a2
Dhruv Mehta <flashxx001@gmail.com>
2024-02-26 22:24:27 +0530

    Some more practice of Project 1. Clear understanding.

--- 07_projects/01_project/script.js
+++ 07_projects/01_project/script.js
@@ -1,17 +1,82 @@
 const body = document.querySelector("body")
 const buttons = document.querySelectorAll(".button")
 
-buttons.forEach(function(button){
+buttons.forEach((button)=>{
     // console.log(button);
-    button.addEventListener("click", function(e){
-        console.log(e);
-        // console.log(e.target);
 
-        if (e.target.id === e.target.id) {
-            body.style.backgroundColor = e.target.id
+    button.addEventListener("click", (clickEvent)=>{
+        console.log(clickEvent);
+        console.log(clickEvent.target.id);
+
+        if (clickEvent.target.id === clickEvent.target.id) {
+            body.style.backgroundColor = clickEvent.target.id           
         }
 
 
+    })
 
-    } )
 })
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// const buttons = document.querySelectorAll(".button")
+// const body = document.querySelector("body")
+
+// // console.log(buttons);
+
+// buttons.forEach(function(button){
+//     // console.log(button);
+
+//     button.addEventListener("click",(e)=>{
+//         // console.log(e);
+//         // console.log(e.target);
+//         if (e.target.id === e.target.id) {
+//             console.log(e.target.id);
+//             body.style.backgroundColor = e.target.id
+//         }
+//     })
+// })
+
+
+
+
+// const body = document.querySelector("body")
+// const buttons = document.querySelectorAll(".button")
+
+// buttons.forEach(function(button){
+//     // console.log(button);
+//     button.addEventListener("click", function(e){
+//         console.log(e);
+//         // console.log(e.target);
+
+//         if (e.target.id === e.target.id) {
+//             body.style.backgroundColor = e.target.id
+//         }
+
+
+
+//     } )
+// })
