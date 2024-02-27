03fb7c376356031469b4e9462ab0a9e68a373678
Dhruv Mehta <flashxx001@gmail.com>
2024-02-27 14:05:03 +0530

    Completed BMI Calculator.

---
+++ 07_projects/02_project/script.js
@@ -0,0 +1,100 @@
+const form = document.querySelector("form")
+
+form.addEventListener('submit', (e)=>{
+    e.preventDefault()
+
+    const height = parseInt(document.querySelector("#height").value)
+    const weight = parseInt(document.querySelector("#weight").value)
+
+    const results = document.querySelector("#results")
+
+    const bmi = (weight / ((height*height)/10000)).toFixed(2)
+
+    if (height === "" || height <=0 || isNaN(height)) {
+        results.innerHTML = `<span>Enter a Valid Height.</span>`
+    }
+    else if (weight === "" || weight <= 0 || isNaN(weight)) {
+        results.innerHTML = `<span>Enter a Valid Weight.</span>`
+    }
+    else{
+        results.innerHTML = `<span>Your BMI is : ${bmi}</span>`
+    }
+    
+    const messages = document.querySelector("#message")
+
+    if(bmi < 18.6){
+        messages.innerHTML = `<span>You are Over Weight</span>`
+    }
+    else if(bmi >= 18.6 && bmi <= 24.9 ){
+        messages.innerHTML = `<span>You are Normal</span>`
+    }
+    else{
+        messages.innerHTML = `<span>You are Overweight. Please Work on it.</span>`
+    }
+
+
+})
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// const form = document.querySelector('form')
+
+// form.addEventListener('submit', (e)=>{
+//     e.preventDefault()
+
+//     const height = parseInt(document.querySelector("#height").value)
+//     const weight = parseInt(document.querySelector("#weight").value)
+//     const results = document.querySelector("#results")
+
+//     if (height === "" || height < 0 || isNaN(height)) {
+//         results.innerHTML = "Please Give a valid Height"
+//     }
+//     else if (weight === "" || weight < 0 || isNaN(weight)) {
+//         results.innerHTML = "Please Give a valid Weight"
+//     } else{
+//         const bmi = (weight / ((height*height)/10000)).toFixed(2)
+//         // show result
+//         results.innerHTML = `<br><span>Your BMI is : ${bmi}</span <br>`
+
+//         const message = document.querySelector("#message")
+
+
+//         if(bmi < 18.6){
+//             message.innerHTML = "<span>You fall in : Under Weight Category</span>"
+//         }
+//         else if (bmi > 18.6 && bmi < 24.9){
+//             message.innerHTML = "<span>Normal</span>"
+//         }
+//         else {
+//             message.innerHTML = "<span>Overweight</span>"
+//         }
+//     }    
+// })
+
