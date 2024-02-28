ff482b4399b06aa5035a9bc698e30fa08928ba4e
Dhruv Mehta <flashxx001@gmail.com>
2024-02-28 14:50:09 +0530

    Practice.

--- 07_projects/03_project/script.js
+++ 07_projects/03_project/script.js
@@ -1,8 +1,18 @@
+// const clock = document.querySelector("#clock")
+
+
+// setInterval(() => {
+//     let date = new Date().toLocaleTimeString();
+//     clock.innerHTML = `${date}`
+// }, 1000);
+
+
 const clock = document.querySelector("#clock")
 
 
-setInterval(() => {
-    let date = new Date().toLocaleTimeString();
-    clock.innerHTML = `${date}`
-}, 1000);
+
+setInterval(()=>{
+    let date = new Date();
+    clock.innerHTML = `${date.toLocaleTimeString()}`
+}, 1000)
 
